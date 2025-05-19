f1ec4ad2d5d8ef303608d17cbe2112627d204acb
anis.kellou@gmail.com <anis.kellou@gmail.com>
2025-05-19 21:50:21 +0000

    Edited settings, Users and ProfilPage

--- client_code/ProfilPage/__init__.py
+++ client_code/ProfilPage/__init__.py
@@ -8,28 +8,42 @@ class ProfilPage(ProfilPageTemplate):
     self.init_components(**properties)
     self.user = anvil.users.get_user()
 
+    self.profil = None
+
     if self.user:
       self.email_label.text = self.user["email"]
 
-      # Récupère le profil dans la table 'profiles'
-      profil = anvil.server.call("get_profil", self.user)
-      if profil:
-        self.name_box.text = profil["name"]
-        self.organisation_box.text = profil["organisation"]
-        self.fonction_box.text = profil["fonction"]
+      self.profil = anvil.server.call("get_profil", self.user)
+
+      if self.profil:
+        self.name_box.text = self.profil["name"]
+        self.fonction_box.text = self.profil["fonction"]
+
+        # Organisation (modifiable uniquement si admin)
+        if self.profil["organisation"]:
+          self.organisation_box.text = self.profil["organisation"]["name"]
+          if self.profil["is_admin"]:
+            self.organisation_box.enabled = True
+          else:
+            self.organisation_box.enabled = False
+        else:
+          self.organisation_box.text = "Aucune"
+          self.organisation_box.enabled = False
+      else:
+        Notification("Profil introuvable.", style="danger").show()
     else:
-      self.email_label.text = "Non connecté"
+      Notification("Utilisateur non connecté.", style="danger").show()
 
   def save_button_click(self, **event_args):
-    """Sauvegarder les modifications de profil"""
+    """Enregistrer les modifications de nom, fonction et organisation si admin"""
     try:
-      anvil.server.call(
-        "update_profil",
-        self.user,
-        self.name_box.text,
-        self.organisation_box.text,
-        self.fonction_box.text
-      )
+      # Nom et fonction
+      anvil.server.call("update_profil", self.user, self.name_box.text, self.fonction_box.text)
+
+      # Organisation si admin
+      if self.profil and self.profil["is_admin"]:
+        anvil.server.call("update_organisation_name", self.profil["organisation"], self.organisation_box.text)
+
       Notification("Profil mis à jour avec succès.", style="success").show()
     except Exception as e:
       Notification(f"Erreur : {e}", style="danger").show()
--- server_code/Users.py
+++ server_code/Users.py
@@ -7,70 +7,65 @@ from anvil.tables import app_tables
 import anvil.server
 
 import stripe
+from datetime import datetime
 
-# Set your secret key. Remember to switch to your live secret key in production.
-# See your keys here: https://dashboard.stripe.com/apikeys
 stripe.api_key = anvil.secrets.get_secret('stripe_secret_api_key')
 
-# @anvil.server.callable's require_user argument takes either a Boolean value. If the Boolean is True, the user has permission to run the decorated function.
-# We can pass require_user a function which evaluates to True or False but we cannot pass any arguments to that function
-# To get around this, we can pass require_user a higher order function - user_has_subscription - which returns an evaluated function verify_subscription
-# user_has_subscription can accept arguments, use them in verify_subscription and return the evaluated verify_subscription function
-# so, if we call @anvil.server.callable(require_user=user_has_subscription(["pro"])), the decorator calls user_has_subscription which returns an evaluated function object that require_user can use
-# verify_subscription receives the currently logged-in user object from @anvil.server.callable automatically
-# See the Product Server Module to see it in use
-def user_has_subscription(allowed_subscriptions):
-    def verify_subscription(user):
-        # Return true if user has subscription in allowed subscriptions
-        return user["subscription"] and user["subscription"].lower() in [subscription.lower() for subscription in allowed_subscriptions]
-    return verify_subscription
+# Créer une organisation + profil lié à l'utilisateur
+@anvil.server.callable
+def enregistrer_profil(user, name, organisation_name, fonction):
+  orga = app_tables.organisations.add_row(
+    name=organisation_name,
+    created_by=user,
+    created_at=datetime.now()
+  )
+  app_tables.profiles.add_row(
+    user=user,
+    name=name,
+    fonction=fonction,
+    organisation=orga,
+    is_admin=True
+  )
 
-
-@anvil.server.callable(require_user=True)
-def change_name(name):
-  user = anvil.users.get_user()
-  user["name"] = name
-  return user
-
-@anvil.server.callable(require_user=True)
-def change_email(email):
-  user = anvil.users.get_user()
-  try:
-    customer = stripe.Customer.modify(
-        user["stripe_id"],
-        email=email
-    )
-    user["email"] = email
-    print("Customer email updated successfully:", customer)
-  except stripe.error.StripeError as e:
-      print("Stripe API error:", e)
-  except Exception as e:
-      print("An error occurred when updating a user's email:", e)
-  return user
-
-@anvil.server.callable(require_user=True)
-def delete_user():
-  user = anvil.users.get_user()
-  if user["stripe_id"]:
-    try: 
-      deleted_customer = stripe.Customer.delete(user["stripe_id"])
-      user.delete()
-      print(f"Customer {user['stripe_id']} deleted successfully:", deleted_customer)
-    except stripe.error.StripeError as e:
-      print("Stripe API error:", e)
-    except Exception as e:
-      print("An unexpected error occurred:", e)
-  else:
-    user.delete()
+# Lire le profil du user
 @anvil.server.callable
 def get_profil(user):
   return app_tables.profiles.get(user=user)
 
+# Mettre à jour le nom + fonction
 @anvil.server.callable
-def update_profil(user, name, organisation, fonction):
+def update_profil(user, name, fonction):
   profil = app_tables.profiles.get(user=user)
   if profil:
     profil["name"] = name
-    profil["organisation"] = organisation
     profil["fonction"] = fonction
 
+# Si admin : modifier le nom de l’organisation
+@anvil.server.callable
+def update_organisation_name(orga_row, new_name):
+  if orga_row:
+    orga_row["name"] = new_name
+
+# Stripe - changement d'email
+@anvil.server.callable(require_user=True)
+def change_email(email):
+  user = anvil.users.get_user()
+  try:
+    customer = stripe.Customer.modify(user["stripe_id"], email=email)
+    user["email"] = email
+  except stripe.error.StripeError as e:
+    print("Stripe API error:", e)
+  except Exception as e:
+    print("Error:", e)
+  return user
+
+# Stripe - suppression utilisateur
+@anvil.server.callable(require_user=True)
+def delete_user():
+  user = anvil.users.get_user()
+  if user["stripe_id"]:
+    try:
+      stripe.Customer.delete(user["stripe_id"])
+    except Exception as e:
+      print("Stripe delete error:", e)
+  user.delete()
