5ae0df1a45c5f11c211fd5e24a92a55611463c48
anis.kellou@gmail.com <anis.kellou@gmail.com>
2025-05-29 07:22:26 +0000

    Created html_graph; Edited OrganigrammeHtml and TestView

--- client_code/TestView/__init__.py
+++ client_code/TestView/__init__.py
@@ -1,24 +1,28 @@
 from ._anvil_designer import TestViewTemplate
 from anvil import *
+from ..OrganigrammeHtml import OrganigrammeHtml  # ✅ Assure-toi que le nom est correct
+
+from anvil.js.window import drawGraph
 
 class TestView(TestViewTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
 
-    # ➕ Données statiques à afficher
-    self.graph_data = {
+    # Exemple de données simples
+    data = {
       "nodes": [
-        {"id": 1, "label": "Société Mère"},
-        {"id": 2, "label": "Filiale A"},
-        {"id": 3, "label": "Filiale B"}
+        {"id": 1, "label": "Node 1"},
+        {"id": 2, "label": "Node 2"},
+        {"id": 3, "label": "Node 3"}
       ],
       "edges": [
         {"from": 1, "to": 2},
-        {"from": 1, "to": 3}
+        {"from": 2, "to": 3}
       ]
     }
 
-  def form_show(self, **event_args):
-    self.call_js("drawGraph", self.graph_data)
+    # Appel de la fonction JS
+    drawGraph(data)
+
 
 
