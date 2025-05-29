baa45aec534dcf9c59d764b8bcfc472a9966c2a9
anis.kellou@gmail.com <anis.kellou@gmail.com>
2025-05-29 07:41:46 +0000

    Edited TestView and html_graph

--- client_code/TestView/__init__.py
+++ client_code/TestView/__init__.py
@@ -1,3 +1,4 @@
+from ._anvil_designer import TestViewTemplate
 from anvil import *
 import anvil.js
 import anvil.js.window
@@ -6,23 +7,55 @@ class TestView(TestViewTemplate):
 
   def __init__(self, **properties):
     self.init_components(**properties)
+    self.inject_visjs_script()
 
-    # Données du graphe
-    data = {
-      "nodes": [
-        {"id": 1, "label": "Départ"},
-        {"id": 2, "label": "Étape 1"},
-        {"id": 3, "label": "Étape 2"}
-      ],
-      "edges": [
-        {"from": 1, "to": 2},
-        {"from": 2, "to": 3}
-      ]
-    }
+  def inject_visjs_script(self):
+    inject_code = """
+      if (!window.visLoaded) {
+        var script = document.createElement('script');
+        script.src = "https://unpkg.com/vis-network/standalone/umd/vis-network.min.js";
+        script.onload = function() {
+          window.visLoaded = true;
+          window.drawGraph = function(data) {
+            var nodes = new vis.DataSet(data.nodes);
+            var edges = new vis.DataSet(data.edges);
+            var container = document.getElementById("mynetwork");
 
-    # Appel de la fonction JS après un court délai (important)
-    anvil.js.call_later(0.1, lambda: anvil.js.window.drawGraph(data))
+            var options = {
+              layout: {
+                hierarchical: {
+                  enabled: true,
+                  direction: "UD"
+                }
+              },
+              physics: false,
+              edges: { arrows: "to" }
+            };
 
+            new vis.Network(container, { nodes: nodes, edges: edges }, options);
+          };
+        };
+        document.head.appendChild(script);
+      }
+    """
+    anvil.js.window.eval(inject_code)
 
-
-
+  def timer_1_tick(self, **event_args):
+    draw_code = """
+      if (window.visLoaded && typeof drawGraph === 'function') {
+        drawGraph({
+          nodes: [
+            {id: 1, label: "Départ"},
+            {id: 2, label: "Étape 1"},
+            {id: 3, label: "Étape 2"}
+          ],
+          edges: [
+            {from: 1, to: 2},
+            {from: 2, to: 3}
+          ]
+        });
+      } else {
+        alert("vis.js n’est pas encore chargé");
+      }
+    """
+    anvil.js.window.eval(draw_code)
