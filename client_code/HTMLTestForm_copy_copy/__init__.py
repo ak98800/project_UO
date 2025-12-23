8081ba64598d2481326ecf9b8a23d782c608abfd
anis.kellou@gmail.com <anis.kellou@gmail.com>
2025-12-23 23:43:10 +0000

    Created HTMLTestForm_copy_copy and VueDescendateView_copy; Edited HTMLTestForm

---
+++ client_code/HTMLTestForm_copy_copy/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import HTMLTestForm_copy_copyTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class HTMLTestForm_copy_copy(HTMLTestForm_copy_copyTemplate):
+  def __init__(self, **properties):
+    self.init_components(**properties)
+
+  def inject_html(self, html_code):
+    print("🔁 HTML injecté dans HTMLTestForm")
+    self.html_container.content = html_code
